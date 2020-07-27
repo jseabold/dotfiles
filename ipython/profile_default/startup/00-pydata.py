432ff6b59240c2cdbf0b7c675e43499f32925769
Skipper Seabold <jsseabold@gmail.com>
2020-07-27 16:07:58 -0500

    More rows

--- ipython/profile_default/startup/00-pydata.py
+++ ipython/profile_default/startup/00-pydata.py
@@ -6,6 +6,6 @@ np.set_printoptions(suppress=True, precision=4)
 
 pd.options.display.float_format = lambda x: f'{x:.4f}'
 pd.options.display.precision = 4
-pd.options.display.max_rows = 10
+pd.options.display.max_rows = 75
 pd.options.display.show_dimensions = False
 pd.options.mode.chained_assignment = None
